9482a112e22f4f445035caae847cea3d9a5cd11c
shams khalid <shamsnaji4@gmail.com>
2020-01-04 20:21:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  // Alternate using ES6 Spread operators:
-  // return { ...driver, ...{ [key]: value } }
+
   return Object.assign({}, driver, { [key]: value });
 }
 
